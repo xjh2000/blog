e3569cc6f238103e44792c8fadb0b29ecdb1ceb6
xjh <934622645@qq.com>
2022-09-27 22:50:42 +0800

    + fix Cannot find module 'node:path' -- vite-plugin-mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,28 @@
 import vue from '@vitejs/plugin-vue'
-import {ConfigEnv, UserConfigExport} from "vite";
-import {viteMockServe} from "vite-plugin-mock";
+import mockServer from 'vite-plugin-mock-server';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+import {defineConfig} from "vite";
 
 const localEnabled = process.env.USE_MOCK || false;
-const mockPlugin = localEnabled ? viteMockServe({
-  // default
-  mockPath: 'mock',
-  localEnabled: true,
+
+const mockPlugin = localEnabled ? mockServer({
+  mockRootDir: './src/mock',
 }) : {} as Plugin;
 
 // https://vitejs.dev/config/
-export default ({command}: ConfigEnv): UserConfigExport => {
-  return {
-    plugins: [
-      vue(),
-      mockPlugin,
-      AutoImport({
-        resolvers: [AntDesignVueResolver()],
-      }),
-      Components({
-        resolvers: [
-          AntDesignVueResolver()
-        ]
-      })
-    ]
-  }
-}
+export default defineConfig({
+  plugins: [
+    vue(),
+    mockPlugin,
+    AutoImport({
+      resolvers: [AntDesignVueResolver()],
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver()
+      ]
+    })
+  ]
+});
