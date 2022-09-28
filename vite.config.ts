4e8ff183a30193b909e66cf72e6f5d2dedeb7c8b
xjh <934622645@qq.com>
2022-09-28 08:18:35 +0800

    fix mock plugin is not a function : because type is module in the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import vue from '@vitejs/plugin-vue'
-import {viteMockServe} from 'vite-plugin-mock';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
 import {defineConfig} from "vite";
+import mockServer from 'vite-plugin-mock-server';
 
 const localEnabled = process.env.USE_MOCK || false;
-const mockPlugin = localEnabled ? viteMockServe({
-  // default
-  mockPath: 'mock',
-  localEnabled: true,
+
+// @ts-ignore module import problem
+const mockPlugin = localEnabled ? mockServer({
+  mockRootDir: './mock',
 }) : {} as Plugin;
 
 // https://vitejs.dev/config/
