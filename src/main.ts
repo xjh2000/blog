32b9aac2351164f330709d9c487defe0a679d81f
xjh <934622645@qq.com>
2022-10-13 20:58:48 +0800

    vite initialization

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from "./router";
-import store from "./store";
 
-let app = createApp(App);
-
-
-// function register
-app.use(router);
-app.use(store);
-
-
-app.mount('#app')
+createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite';
-import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
-import {defineConfig} from "vite";
-import mockServer from 'vite-plugin-mock-server';
-
-const localEnabled = process.env.USE_MOCK || false;
-
-// @ts-ignore module import problem
-const mockPlugin = localEnabled ? mockServer({
-  mockRootDir: './src/mock',
-}) : {} as Plugin;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    mockPlugin,
-    AutoImport({
-      resolvers: [AntDesignVueResolver()],
-    }),
-    Components({
-      resolvers: [
-        AntDesignVueResolver()
-      ]
-    })
-  ]
-});
+  plugins: [vue()]
+})
