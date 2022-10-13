ae6e76694a41ad0dbf1a079d0aadcf7140e9601d
xjh <934622645@qq.com>
2022-10-13 21:06:14 +0800

    delete before files

--- src/router/base.ts
+++
@@ -1,7 +0,0 @@
-import {RouteRecordRaw} from "vue-router";
-
-export const BASIC_ROUTER: RouteRecordRaw[] = [
-  {path: '/', redirect: '/home'},
-  {path: '/home', name: 'home', component: () => import("../views/HomeView.vue")},
-  {path: '/about', component: () => import("../views/AboutView.vue")},
-]
