a7e26698fbdb2e7668714e81a9ee73162d8e9c38
xjh <934622645@qq.com>
2023-02-17 22:05:01 +0800

    add redis and mongo db config

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,16 +4,24 @@
  * @LastEditTime: 2021-09-10 20:16:17
  */
 
-
+let db = process.env.NODE_ENV === 'prod' ?
+    {
+        host: 'mongo',
+        port: 27017,
+        user: 'root',
+        password: 'mongo',
+        database: 'blog'
+    } :
+    {
+        host: '192.168.2.101',
+        port: 27017,
+        user: 'root',
+        password: 'mongo',
+        database: 'blog'
+    };
 
 
 // MongoDB配置
 module.exports = {
-    db: {
-        host: '127.0.0.1', // 连接地址
-        port: 27017, // 端口
-        user: '', // 用户名
-        password: '', // 密码
-        database: '' // 数据库名
-    }
+    db: db
 }
--- config/redis.js
+++ config/redis.js
@@ -4,11 +4,20 @@
  * @LastEditTime: 2021-04-27 23:33:44
  */
 
+let db = process.env.NODE_ENV === 'prod' ?
+    {
+        host: 'redis',
+        port: 6379,
+        db: 0
+    } :
+    {
+        host: '192.168.2.101',
+        port: 6379,
+        db: 0
+    };
+
+
 // redis配置
 module.exports = {
-    db: {
-        host: '127.0.0.1',
-        prot: 6379,
-        db: 0
-    }
+    db: db
 }
