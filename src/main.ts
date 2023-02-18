fa210b1d8327c4c75b7f5a9244de7c8ad44c1788
xjh <934622645@qq.com>
2023-02-18 15:05:04 +0800

    let the docker image to small

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExceptionFilter } from './handler/exception/exception.filter';
+import { RespondInterceptor } from './handler/respond/respond.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 统一异常处理
+  app.useGlobalFilters(new ExceptionFilter());
+  // 统一正常数据的返回
+  app.useGlobalInterceptors(new RespondInterceptor());
 
   await app.listen(8080);
 }
 
-bootstrap();
+bootstrap().then((r) => console.log(r));
---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    // throw new BadRequestException({ detail: 'the user haven been register' });
+    throw new BadRequestException('the user haven been register');
+
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
